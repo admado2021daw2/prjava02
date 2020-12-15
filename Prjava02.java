7a9ce5d0351c6fc8f151d19ba3dae443f83d0468
admado2021daw2 <adrian98do@gmail.com>
2020-12-15 20:55:29 +0100

    Versió 1.3 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -20,6 +20,7 @@ public class Prjava02 {
         System.out.println("versiÃ³ 0.1 del projecte prjava02"); 
         System.out.println("Creacio? d'una branca del projecte prjava02");
         System.out.println("Afegint me?s codi a la branca00 del projecte prjava02");
+        System.out.println("Afegint codi a la branca01 del projecte prjava02");
         try {
             InetAddress adreça = InetAddress.getLocalHost();
             String hostname = adreça.getHostName();
